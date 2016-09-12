30328d5995da14dfab8109481cf1613a48eb6975
rob boll <robertcboll@gmail.com>
2016-09-11 21:01:37 -0400

    no podmaster

--- modules/controller/aws/images.tf
+++ modules/controller/aws/images.tf
@@ -3,43 +3,12 @@ resource docker_image hyperkube {
     keep_locally = true
 }
 
-resource dockerx_build podmaster {
-    name = "${var.env}-podmaster"
-    context_dir = "${replace("${path.module}/../podmaster/docker/", "${path.root}", ".")}"
-}
-
 resource aws_ecr_repository hyperkube {
     name = "${var.env}-kube-controller-hyperkube"
 }
 
-resource aws_ecr_repository podmaster {
-    name = "${dockerx_build.podmaster.name}"
-}
-
 resource aws_ecr_repository_policy hyperkube {
     repository = "${aws_ecr_repository.hyperkube.name}"
-    policy = <<EOF
-{
-    "Version": "2008-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "AWS": [ "${aws_iam_role.kube_controller.arn}" ]
-            },
-            "Action": [
-                "ecr:GetDownloadUrlForLayer",
-                "ecr:BatchGetImage",
-                "ecr:BatchCheckLayerAvailability"
-            ]
-        }
-    ]
-}
-EOF
-}
-
-resource aws_ecr_repository_policy podmaster {
-    repository = "${aws_ecr_repository.podmaster.name}"
     policy = <<EOF
 {
     "Version": "2008-10-17",
@@ -67,13 +36,4 @@ resource ecr_push hyperkube {
     repo = "${replace(aws_ecr_repository.hyperkube.repository_url, "/^https://(.*)/.*$/", "$1")}"
 
     depends_on = [ "docker_image.hyperkube", "aws_ecr_repository_policy.hyperkube" ]
-}
-
-resource ecr_push podmaster {
-    image = "${dockerx_build.podmaster.image}"
-    tag = "${dockerx_build.podmaster.tag}"
-    name = "${aws_ecr_repository.podmaster.name}"
-    repo = "${replace(aws_ecr_repository.podmaster.repository_url, "/^https://(.*)/.*$/", "$1")}"
-
-    depends_on = [ "aws_ecr_repository_policy.podmaster" ]
 }
