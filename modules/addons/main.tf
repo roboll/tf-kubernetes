f906bc2c7f42d34e3ce827903e7059e1fa54c2ab
rob boll <robertcboll@gmail.com>
2016-09-13 03:32:48 -0400

    try this

--- modules/addons/main.tf
+++ modules/addons/main.tf
@@ -15,20 +15,22 @@ provider ecr {
 
 resource null_resource dir {
     provisioner local-exec {
-        command = "mkdir -p ${path.root}/kube && \
-            echo ${data.template_file.access.rendered} > ${path.root}/kube/access.yaml && \
-            echo ${data.template_file.dashboard.rendered} > ${path.root}/kube/kube-dashboard.yaml && \
-            echo ${data.template_file.heapster.rendered} > ${path.root}/kube/heapster.yaml && \
-            echo ${data.template_file.addon_manager.rendered} > ${path.root}/kube/kube-addon-manager.yaml && \
-            echo ${data.template_file.controller_manager.rendered} > ${path.root}/kube/kube-controller-manager.yaml && \
-            echo ${data.template_file.controller_metrics.rendered} > ${path.root}/kube/controller-metrics.yaml && \
-            echo ${data.template_file.ingress.rendered} > ${path.root}/kube/kube-ingress.yaml && \
-            echo ${data.template_file.proxy.rendered} > ${path.root}/kube/kube-proxy.yaml && \
-            echo ${data.template_file.scheduler.rendered} > ${path.root}/kube/kube-scheduler.yaml && \
-            echo ${data.template_file.logging.rendered} > ${path.root}/kube/logging.yaml && \
-            echo ${data.template_file.alerts_config.rendered} > ${path.root}/kube/metrics-alerts-config.yaml && \
-            echo ${data.template_file.metrics_config.rendered} > ${path.root}/kube/metrics-config.yaml && \
-            echo ${data.template_file.metrics.rendered} > ${path.root}/kube/metrics.yaml"
+        command = <<EOF
+mkdir -p ${path.root}/kube && \
+echo ${data.template_file.access.rendered} > ${path.root}/kube/access.yaml && \
+echo ${data.template_file.dashboard.rendered} > ${path.root}/kube/kube-dashboard.yaml && \
+echo ${data.template_file.heapster.rendered} > ${path.root}/kube/heapster.yaml && \
+echo ${data.template_file.addon_manager.rendered} > ${path.root}/kube/kube-addon-manager.yaml && \
+echo ${data.template_file.controller_manager.rendered} > ${path.root}/kube/kube-controller-manager.yaml && \
+echo ${data.template_file.controller_metrics.rendered} > ${path.root}/kube/controller-metrics.yaml && \
+echo ${data.template_file.ingress.rendered} > ${path.root}/kube/kube-ingress.yaml && \
+echo ${data.template_file.proxy.rendered} > ${path.root}/kube/kube-proxy.yaml && \
+echo ${data.template_file.scheduler.rendered} > ${path.root}/kube/kube-scheduler.yaml && \
+echo ${data.template_file.logging.rendered} > ${path.root}/kube/logging.yaml && \
+echo ${data.template_file.alerts_config.rendered} > ${path.root}/kube/metrics-alerts-config.yaml && \
+echo ${data.template_file.metrics_config.rendered} > ${path.root}/kube/metrics-config.yaml && \
+echo ${data.template_file.metrics.rendered} > ${path.root}/kube/metrics.yaml
+EOF
     }
 }
 
