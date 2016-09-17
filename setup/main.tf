f8bce3bc57aa79291add0853aab19a857d12737f
rob boll <robertcboll@gmail.com>
2016-09-17 16:43:24 -0400

    work

--- setup/main.tf
+++ setup/main.tf
@@ -106,8 +106,8 @@ data template_file kube_controller {
 
     vars {
         etcd_pki_backend = "${var.etcd_pki_backend}"
-        etcd_vault_role_id = "${vaultx_secret.etcd_role_id.data["role_id"]}"
-        etcd_vault_secret_id = "${vaultx_secret.etcd_secret_id.data["secret_id"]}"
+        etcd_vault_role_id = "${vaultx_secret.etcd_role_id.data[role_id]}"
+        etcd_vault_secret_id = "${vaultx_secret.etcd_secret_id.data[secret_id]}"
     }
 }
 
