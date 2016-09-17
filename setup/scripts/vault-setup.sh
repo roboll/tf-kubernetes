e4bd01eb95162a740b68b81ee6c5c4ccd664dd4d
rob boll <robertcboll@gmail.com>
2016-09-17 17:38:39 -0400

    work

--- setup/scripts/vault-setup.sh
+++ setup/scripts/vault-setup.sh
@@ -5,8 +5,8 @@ namespace="${namespace}"
 
 role_id="$(vault read -field=role_id auth/approle/role/$role/role-id)"
 kubectl create configmap $${role}-vault-role --namespace=kube-system \
-    --from-literal-value=role_id=$role_id
+    --from-literal=role_id=$role_id
 
 secret_id="$(vault write -f -field=secret_id auth/approle/role/$role/secret-id)"
 kubectl create secret generic $${role}-vault-secret \
-    --from-literal-value=secret_id=$secret_id --namespace=kube-system
+    --from-literal=secret_id=$secret_id --namespace=kube-system
