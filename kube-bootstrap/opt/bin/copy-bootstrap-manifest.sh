edba81db7b44477b084dbbf5d88c20c563f65844
rob boll <robertcboll@gmail.com>
2016-10-30 23:51:31 -0400

    close to self hosted with vault controller

--- kube-bootstrap/opt/bin/copy-bootstrap-manifest.sh
+++ kube-bootstrap/opt/bin/copy-bootstrap-manifest.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 set -eo pipefail
 
-vars='${VAULT_ADDR} ${KUBE_FQDN} ${ETCD_INITIAL_CLUSTER} ${ETCD_PKI_MOUNT} ${KUBE_PKI_MOUNT} ${SVC_ACCT_PUBKEY} ${SVC_ACCT_PRIVKEY} ${HYPERKUBE}'
-
+vars='${KUBE_FQDN} ${HYPERKUBE} ${ETCD_PEERS} ${ETCD_NODES}'
 cat /etc/kube-bootstrap/bootstrap.yaml | envsubst "$vars" > /etc/kubernetes/manifests/bootstrap.yaml
--- kube-bootstrap/opt/bin/kube-bootstrap.sh
+++ kube-bootstrap/opt/bin/kube-bootstrap.sh
@@ -2,6 +2,7 @@
 set -eo pipefail
 
 type_apis=(
+    "thirdpartyresource:http://localhost:8080/apis/extensions/v1beta1/thirdpartyresources"
     "clusterrole:https://kubernetes/apis/rbac.authorization.k8s.io/v1alpha1/clusterroles"
     "clusterrolebinding:https://kubernetes/apis/rbac.authorization.k8s.io/v1alpha1/clusterrolebindings"
     "configmap:http://localhost:8080/api/v1/namespaces/kube-system/configmaps"
@@ -10,12 +11,13 @@ type_apis=(
     "serviceaccount:http://localhost:8080/api/v1/namespaces/kube-system/serviceaccounts"
     "deployment:http://localhost:8080/apis/extensions/v1beta1/namespaces/kube-system/deployments"
     "daemonset:http://localhost:8080/apis/extensions/v1beta1/namespaces/kube-system/daemonsets"
+    "secretclaim:http://localhost:8080/apis/vaultproject.io/v1/namespaces/kube-system/secretclaims"
 )
 ds_api="http://localhost:8080/apis/extensions/v1beta1/namespaces/kube-system/daemonsets"
 node_api="http://localhost:8080/api/v1/nodes"
 healthz_api="http://localhost:8080/healthz"
 
-vars='${VAULT_ADDR} ${KUBE_FQDN} ${ETCD_INITIAL_CLUSTER} ${ETCD_PKI_MOUNT} ${KUBE_PKI_MOUNT} ${SVC_ACCT_PUBKEY} ${SVC_ACCT_PRIVKEY} ${HYPERKUBE}'
+vars='${VAULT_ADDR} ${KUBE_FQDN} ${ETCD_NODES} ${ETCD_PEERS} ${ETCD_PKI_MOUNT} ${KUBE_PKI_MOUNT} ${SVC_ACCT_PUBKEY} ${SVC_ACCT_PRIVKEY} ${HYPERKUBE}'
 
 opts="-sSfk --resolve kubernetes:443:127.0.0.1"
 json="Content-Type: application/json"
@@ -23,8 +25,12 @@ auth="Authorization: Bearer bootstrap"
 manifests=/etc/kube-bootstrap/manifests
 api_objects=/etc/kube-bootstrap/api-objects
 
+if [ -f /etc/kube-bootstrap/env ]; then
+    . /etc/kube-bootstrap/env
+fi
+
 bootstrap() {
-    if [ $(curl $opts $healthz_api) ] && [ curl $opts $node_api >/dev/null ]; then
+    if [ $(curl $opts $healthz_api) ] && [ $(curl $opts $node_api >/dev/null) ]; then
         echo "apiserver up, not copying bootstrap manifests"
     else
         echo "copying bootstrap manifests to /etc/kubernetes/manifests..."
@@ -48,13 +54,14 @@ bootstrap() {
             echo ""
             echo "creating $api_type from $file"
             cat $file | envsubst "$vars" | curl $opts -H "$auth" -XPOST -H "$json" -d@- $api_path || true
+            sleep 1
         done
     done
 
     echo ""
     echo "waiting for daemonsets to schedule"
-    for ds in $manifests/*.yaml; do
-        ds_name=$(echo $ds | sed -e s,$manifests/,,g -e s,.yaml,,g -e s,bootstrap-,,g)
+    for ds in $manifests/bootstrap-ds-*.yaml; do
+        ds_name=$(echo $ds | sed -e s,$manifests/,,g -e s,.yaml,,g -e s,bootstrap-ds-,,g)
 
         echo "checking daemonset $ds_name"
         until [ $(curl $opts -H "$auth" $ds_api/$ds_name | jq .status.currentNumberScheduled) -gt 0 ]; do
@@ -77,7 +84,7 @@ bootstrap
 
 while true; do
     sleep 60
-    if [ $(curl $opts $healthz_api) ] && [ $(curl $opts $node_api) >/dev/null ]; then
+    if [ $(curl $opts $healthz_api) ] && [ $(curl $opts $node_api >/dev/null) ]; then
         echo "apiserver ok, sleeping 60s"
     else
         bootstrap
